e4b8ebc01216dd34470dbb075c5dc0eb89170247
Sonia Martin <smartinr12@gmail.com>
2011-11-08 19:24:06 +0100

    first commit

---
+++ src/com/uoc/lsviewer/LSViewerActivity.java
@@ -0,0 +1,13 @@
+package com.uoc.lsviewer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LSViewerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
