430cbc779b090bf76e4224d94c81c179b155032e
Sonia Martin <smartinr12@gmail.com>
2011-11-09 20:57:25 +0100

    add maps class

---
+++ src/com/uoc/lsviewer/MapsList.java
@@ -0,0 +1,13 @@
+package com.uoc.lsviewer;
+
+import greendroid.app.GDMapActivity;
+
+public class MapsList extends GDMapActivity {
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
