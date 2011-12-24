7f8addf16ec59c7bcb73a7d59ec2ba159065433a
Sonia Martin <smartinr12@gmail.com>
2011-12-24 12:26:01 +0100

    ImageSensors class working

--- src/com/uoc/lsviewer/ImageSensors.java
+++ src/com/uoc/lsviewer/ImageSensors.java
@@ -1,39 +1,38 @@
 package com.uoc.lsviewer;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.drawable.Drawable;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Display;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.ImageView;
 import android.widget.Toast;
 import greendroid.app.GDActivity;
 
 public class ImageSensors extends GDActivity{
 
-	private InternalView myView;
-	private ServerConnection sc;
+	private InternalView myView;	// Internal class to draw
+	private ServerConnection sc;	// Connection to server
 	private String url;
 	private String idIMG;
 	private String session;
 	private Context context;
+	private int selX;		// X index of selection
+	private int selY;		// Y index of selection
+	private ArrayList<Dot> al;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -45,7 +44,7 @@ public class ImageSensors extends GDActivity{
 		session = bundle.getString("session");
 		idIMG = bundle.getString("index");
 		url = bundle.getString("url");
-		
+
 		// Create view (canvas)
 		myView = new InternalView(this);
 		setActionBarContentView(myView);
@@ -67,19 +66,22 @@ public class ImageSensors extends GDActivity{
 				url_value = new URL(url);
 				mIcon1 = BitmapFactory.decodeStream(url_value.openConnection().getInputStream());
 			} catch (IOException e1) {
-				 Log.e("log_tag", "Error in http connection "+e1.toString());
-			}			          
-			
+				Log.e("log_tag", "Error in http connection "+e1.toString());
+			}			 
+
+			Paint background = new Paint();
+			background.setColor(getResources().getColor(R.color.greyWeak));
+			canvas.drawRect(0, 0, getWidth(), getHeight(), background);
+
 			// Resize image
 			Bitmap resized = null;
-			Display display = getWindowManager().getDefaultDisplay();                 
-		
+
 			int imgHeight = mIcon1.getHeight();
 			int imgWidth = mIcon1.getWidth();
-			double escalaX = (double)display.getWidth() / (double)imgWidth;
-			double escalaY = (double)display.getHeight() / (double)imgHeight;
+			double escalaX = (double) getWidth() / (double)imgWidth;
+			double escalaY = (double) getHeight() / (double)imgHeight;
 
-			// Tomo como referencia el minimo de las escalas
+			// Get mininum scale
 			double fEscala = Math.min(escalaX, escalaY);
 			if(fEscala > 1) {
 				fEscala = 1;
@@ -90,8 +92,7 @@ public class ImageSensors extends GDActivity{
 			resized = Bitmap.createScaledBitmap(mIcon1, imgWidth, imgHeight, false); 			
 
 			Paint paint = new Paint();  
-			//canvas.drawColor(R.color.greyWeak);
-						
+
 			// paint image
 			canvas.drawBitmap(resized, 0, 0, paint);
 
@@ -110,31 +111,86 @@ public class ImageSensors extends GDActivity{
 				// Calcule the point of the dot
 				int xDot = dot.getHeight() / 2;
 				int yDot = dot.getWidth();
-				
+
+				al = new ArrayList<Dot>();
+
 				// Get data
 				for(int i = 0; i < jArray.length(); i++){	        	
 					JSONObject json_data = jArray.getJSONObject(i);
-
-					float x =  (float) (json_data.getInt("x") * fEscala) - xDot;
-					float y =  (float) (json_data.getInt("y") * fEscala) - yDot; 
+										
+					int xP = (int) (json_data.getInt("x") * fEscala);
+					int yP = (int) (json_data.getInt("y") * fEscala);
+					Rect dotRect = new Rect(xP - 11, yP - 32, xP + 11, yP - 5);
 					
+					// Paint rect OnTouch
+					//canvas.drawRect(dotRect, paint);
+					
+					// Save Dot class
+					al.add(new Dot(json_data.getString("sensor"), xP, yP, dotRect));
+
+					float x =  (float) xP - xDot;
+					float y =  (float) yP - yDot; 
+
 					// Paint sensor
 					canvas.drawBitmap(dot, x, y, paint);
-					//images.add(server + "Imatges/" + json_data.getString("imatge"));
-					//idImages.add(json_data.getString("IdImatge"));					
 				}   
-				//Toast.makeText(getApplicationContext(), "pass3", Toast.LENGTH_SHORT).show();
-				//imageURLs = images.toArray(new String[images.size()]);
-				//idIMG = idImages.toArray(new String[idImages.size()]);
 
 			}catch(JSONException e){
 				Log.e("log_tag", "Error parsing data "+e.toString());
 				Toast.makeText(getApplicationContext(), "fail3", Toast.LENGTH_SHORT).show();
 			}
-
-
-
-
 		}
+
+		@Override
+		public boolean onTouchEvent(MotionEvent event) {
+			if (event.getAction() != MotionEvent.ACTION_DOWN)
+				return super.onTouchEvent(event);
+
+			selX = (int) event.getX();
+			selY = (int) event.getY();
+			boolean in = false;
+			String idSensor = null;
+			for(int i = 0; i < al.size(); i++) {
+				Dot d = al.get(i);
+				Rect rect = d.getDotRect();
+				
+				if(rect.contains(selX, selY)) {
+					//Toast.makeText(getApplicationContext(), "YES! onTouchEvent: x " + event.getX() + ", y " + event.getY(), Toast.LENGTH_SHORT).show();
+					in = true;
+					idSensor = d.getId();
+				}
+			}
+			
+			if (in){
+				Intent intent = new Intent(ImageSensors.this, SensorInfo.class);
+				Bundle bundle = new Bundle();
+				bundle.putString("session", session);
+				bundle.putString("sensor", idSensor);
+				intent.putExtras(bundle);
+				startActivity(intent);
+			}
+			return in;
+		}
+	}
+
+	class Dot
+	{
+		String id;
+		int x;
+		int y;
+		Rect dotRect;
+
+		Dot(String id, int x, int y, Rect dotRect) { 
+			this.id = id; 
+			this.x = x;
+			this.y = y;
+			this.dotRect = dotRect;
+		}
+
+		public String getId() { return id; }
+		public int getX() { return x; }
+		public int getY() { return y; }
+		public Rect getDotRect() { return dotRect; }
+
 	}
 }
