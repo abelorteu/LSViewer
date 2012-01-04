03c5be7a176170a97fc1ff52b36bd940cae8ed04
abelorteu <abelorteu@gmail.com>
2012-01-04 18:47:07 +0100

    Pàgina d'ajuda

--- src/com/uoc/lsviewer/Help.java
+++ src/com/uoc/lsviewer/Help.java
@@ -1,13 +1,16 @@
 package com.uoc.lsviewer;
 
-import android.os.Bundle;
 import greendroid.app.GDActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
 
 public class Help extends GDActivity {
-	
+	WebView webHelp;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setActionBarContentView(R.layout.help);
+		webHelp=(WebView)findViewById(R.id.Help);
+		webHelp.loadUrl(getResources().getString(R.string.Servidor) + getResources().getString(R.string.Help));
 	}
 }
